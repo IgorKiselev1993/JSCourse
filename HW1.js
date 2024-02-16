5b1c1eeb4b84487415f5b1125a7a34c6725351e3
IgorKiselev1993 <kis-son@mail.ru>
2024-02-16 20:24:24 +0200

    ss

--- HW1.js
+++ HW1.js
@@ -2,3 +2,13 @@ function showMessage() {
     console.log("Hello JSCourse!");
 }
 showMessage()
+
+const user = {
+    name: "Boris",
+    age: 24
+}
+
+user['firstName'] = user.name
+delete user.nameпше
+
+console.log(user)
